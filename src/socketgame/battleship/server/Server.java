60ff464d92d3dda40482bbc1014c83f8be23bfe3
davidq2010 <36721469+davidq2010@users.noreply.github.com>
2018-07-08 20:22:05 -0400

    Server

--- src/socketgame/battleship/server/Server.java
+++ src/socketgame/battleship/server/Server.java
@@ -1,5 +1,32 @@
 package socketgame.battleship.server;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class Server {
+	static final int PORT = 9999;
+	private static ExecutorService executorService;
+
+	public static void main(String[] args) {
+		try (
+			ServerSocket serverSocket = new ServerSocket(PORT);
+		) {
+			executorService = Executors.newCachedThreadPool();
+			while(true) {
+				try (
+					Socket clientSocket = serverSocket.accept();
+				) {
+					ClientHandler handler = new ClientHandler(clientSocket);
+					executorService.submit(handler);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}	
+		} catch (IOException e) {
+			e.printStackTrace();
+		}				
+	}
 }
